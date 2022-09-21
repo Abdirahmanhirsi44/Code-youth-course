dc20a9b22fbab6132bec34c4699e94e872db4b2f
Abdirahmanhirsi44 <111757037+Abdirahmanhirsi44@users.noreply.github.com>
2022-09-21 12:51:22 -0400

    arrays
    learning array concepts like splice and push

---
+++ html/arraypush.js
@@ -0,0 +1,43 @@
+// let arr1 = ['1','2','3'];
+// arr1.push('4');
+// console.log(arr1)
+
+// let arr2 = ["a","b","c"];
+// arr2.push("d");
+// console.log(arr2);
+
+
+// let arry3 = []
+// for( i = 0; i<=10; i++){
+// arry3.push(i)
+// }
+// console.log(arry3)
+
+
+// let arr4 = [4,5,6,7];
+// arr4.pop()
+// console.log(arr4)
+
+// let arr5 = ['f','g','h','i']
+// arr5.pop()
+// console.log(arr5)
+
+
+// let arr6 = ['cats','dog','bird']
+// arr6.shift()
+// console.log(arr6)
+
+// let arr7 = ['cake','cookies','ice cream']
+// arr7.unshift('salad')
+// console.log(arr7)
+
+let weekdays = ["Friday", "Tuesday","Wednesday",
+"Thursday","Sunday","Monday","Saturday","PlaceHolder"]
+weekdays.pop()
+weekdays.shift()
+weekdays.unshift('monday')
+weekdays.pop()
+weekdays.pop()
+weekdays.pop()
+weekdays.push('friday','saturday','sunday')
+console.log(weekdays)
---
+++ html/arraysplice.js
@@ -0,0 +1,67 @@
+// let arr1 = [1,2,3,4,5]
+// arr1.splice(0,3)
+// console.log(arr1);
+
+// let arr2 = [1, 2, 3, 4, 5]
+// arr2.splice(0,3);
+// console.log(arr2);
+
+// let refridge = ["Old bread","Banana","pizza","cucumber",
+// "icecream","Pasta","hotdog"]
+// refridge.splice(0,1)
+// refridge.splice(1,2)
+// console.log(refridge)
+
+
+// let arr3 = [1,2,3];
+// let arr4 = [4,5,6];
+// console.log(arr3);
+// console.log(arr4);
+// let arr5 = arr3.concat(arr4);
+// console.log(arr5);
+
+
+// let a1 = ['a','b','c']
+// let a2 = ['d','e','f']
+// console.log(a1)
+// console.log(a2)
+// let a3 = a1.concat(a2);
+// console.log(a3)
+
+
+// let b1 = [1,2,3,4,5]
+// let b2 = b1.slice(0,3)
+// console.log("array 1", b1)
+// console.log("arry 2", b2)
+
+// let c1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+// let c2 = c1.slice(4,7);
+// console.log("array 1",c1)
+// console.log("array 2",c2)
+
+// let arr1 = [1,2,3,4,5]
+// console.log(arr1)
+// console.log(arr1.includes(2));
+
+// let arr2 = ['cat','dog','bird']
+// console.log(arr2)
+// console.log(arr2.includes("dog"));
+
+let d1 = [1,2,3,4,5]
+console.log(d1)
+console.log(d1.includes(4))
+
+
+// let nums = [];
+//     for(let i = 0; i < 51; i++){
+//         nums.push(Math.floor(Math.random()*100))
+//     }
+
+//     if 
+
+
+
+
+
+
+
