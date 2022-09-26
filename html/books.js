269894f352be7d07d0064387cdd607d641b45405
Abdirahmanhirsi44 <111757037+Abdirahmanhirsi44@users.noreply.github.com>
2022-09-26 12:51:36 -0400

    movies.js
    learning arrays in objects

---
+++ html/books.js
@@ -0,0 +1,74 @@
+// let a = [12,23,45,66,2];
+// for (let i=0; i<a.length;i++){
+//     console.log(a[i]);
+// }
+
+// for (let j of a ){
+//     console.log(j)
+
+// }
+
+// for (let j in a){
+//     console.log(j, "=",a[j]);
+
+// }
+
+// let count = 0
+// let person = {
+//     FirstName : "Jimmy",
+//     LastName: "Ben",
+//     PhoneNumber: 6132256541,
+//     Email:"JimmyBen@gmail.com"
+// }
+// }
+//  for (let i = 0; i<5; i++){
+//     console.log
+//  }
+
+// for (let key in person){
+//             console.log(key, "=", person[key])
+//             count++;
+// }
+            
+//             console.log(count)
+
+let a = [1,3,67,89];
+for (let i=0; i<a.length;i++){
+        console.log(a[i]);}
+
+       
+ let student = [
+
+    {
+        id : 1,
+        name: "bob",
+        address: "toronto"
+ 
+    }
+,
+{
+    id : 2,
+        name: "Kobe",
+        address: "ottawa"
+}
+,
+
+{
+    id : 3,
+        name: "jim",
+        address: "kanata"
+}
+
+,
+{
+
+    id : 4,
+        name: "paul",
+        address: "newyork"
+}
+]
+
+
+for (let j=0; j<student.length;j++){
+    console.log(student[j].address)
+}
---
+++ html/movies.js
@@ -0,0 +1,75 @@
+// let movies = [
+
+//     {
+//         title : "Ironman",
+//         author: "Marvel",
+//         reading: true
+ 
+//     }
+// ,
+// {
+//     title : "Superman",
+//         author: "DC",
+//         reading: false
+// }
+// ,
+
+// {
+//     title : "LionKing",
+//        author: "Disney",
+//         reading: true
+// }
+
+// ,
+// ]
+
+// for (let a=0; a<movies.length;a++){
+//     console.log(" The movie " + movies[a].title + " by " + movies[a].author)
+
+// if (movies[a].reading ==false){
+//     console.log("I have not read it yet", movies[a].title)}
+// }
+
+
+
+// var list = [
+//     {
+//        firstName: "Kiran",
+//        lastName : "Kumar",
+//        salary  : 25000
+//        },
+//     {
+//        firstName: "Mohan",
+//        lastName : "Srivastav",
+//        salary  : 20000
+//       },
+//     {
+//        firstName: "Mary",
+//        lastName : "Kom",
+//        salary   : 23000
+//        }
+//       ];
+      
+//       for (let a=0; a<list.length;a++)
+//       if (list[a].salary>22500)
+//       {console.log("The salary of "+list[a].firstName,list[a].lastName +" is "+ list[a].salary,)
+
+//       }
+
+
+let myNums = [{"x": 9,"y": 5},
+{"x": 6,"y": 3},
+{"x": 7,"y": 7},{"x": 5,"y": 5},
+{"x": 0,"y": 3},{"x": 7,"y": 3},{"x": 4,"y": 9},
+{"x": 8,"y": 8},{"x": 5,"y": 3},{"x": 1,"y": 7},
+{"x": 7,"y": 5},{"x": 7,"y": 7},{"x": 6,"y": 0},
+{"x": 8,"y": 3},{"x": 1,"y": 3},{"x": 3,"y": 2},
+{"x": 9,"y": 3},{"x": 7,"y": 6},{"x": 7,"y": 1},
+{"x": 1,"y": 7}]
+ 
+let count = 0
+for ( let i=0; i<myNums.length; i++){
+    if (myNums[i].x == myNums[i].y)
+    count++
+}
+console.log(count)
